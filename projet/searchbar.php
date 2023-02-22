7482b596054c8579df4ba3a4e987f07fa0c7c69b
NolwenMaj <115166022+NolwenMaj@users.noreply.github.com>
2023-02-22 15:46:11 +0100

    likes and searchbar updated

--- projet/searchbar.php
+++ projet/searchbar.php
@@ -5,39 +5,99 @@
             <?php
             if (isset($_POST['submit-search'])){
                 $search = mysqli_real_escape_string($mysqli, $_POST['search']);
-                $sql ="SELECT * FROM boardgames WHERE name LIKE '%$search%' OR type 
-                    LIKE '%$search%' OR description LIKE '%$search%' OR creator LIKE '%$search%'";
-                $result = mysqli_query($mysqli,$sql);
-                $queryResult = mysqli_num_rows($result);
 
-                if ($queryResult > 0){
-                    echo "Resultats de votre recherche :" ;
-                   while ($row = mysqli_fetch_assoc($result))
-                   { 
+                $sqlTabBoardgames ="SELECT * FROM boardgames 
+                WHERE name LIKE '%$search%' 
+                OR type LIKE '%$search%' 
+                OR description LIKE '%$search%' 
+                OR creator LIKE '%$search%' ";
+                $resultTabBoardgames = mysqli_query($mysqli,$sqlTabBoardgames);
+                $queryResultBoardgames =mysqli_num_rows($resultTabBoardgames);
+
+                $sqlTabUsers ="SELECT * FROM users 
+                WHERE alias LIKE '%$search%' 
+                OR email LIKE '%$search%'";
+                $resultTabUsers = mysqli_query($mysqli,$sqlTabUsers);
+                $queryResultUsers = mysqli_num_rows($resultTabUsers);
+
+                $sqlTabPosts ="SELECT * FROM posts 
+                WHERE content LIKE '%$search%'";
+                $resultTabPosts = mysqli_query($mysqli,$sqlTabPosts);
+                $queryResultPosts = mysqli_num_rows($resultTabPosts);
+
+
+                $queryResultAll = $queryResultBoardgames + $queryResultUsers + $queryResultPosts;
+
+                if ($queryResultAll > 0){
                     ?>
-                        <article>
-                        <section id='articleJeux'>
-                            <div>
-                                <h1><?=$row['name']?></h1>
-                                <small>Créateur•ice : <?=$row['creator']?></small>
-                                <p><strong>Age minimum : <?=$row['min_age']?>ans • Nombre de joueur•euse•s : <?=$row['min_players']?> - <?=$row['max_players']?> • Durée d'une partie : <?=$row['duration']?>minutes </strong></p> 
-                                <br>
-                                <p><?=$row['description']?></p>
-                            </div>
-                            <br>
-                            <div>
-                                <img alt='Image du jeu<?=$row['name']?>' height='300' src = <?=$row['images']?>></img>
-                            </div>
-                        </section>
-                        <footer>
-                            <form action="addToLudotheque.php" method="POST">
-                                 <button type="submit" name="submit-like" value=<?=$row['id']?> >♥</button>
-                            </form>
-                            <p>Type: <i><?=$row['type']?></i></p>  
-                        </footer>
-                    </article>
+                    <h1> Resultats de votre recherche : </h1>
                     <?php
-                   }
+                    if ($queryResultUsers > 0){
+                        ?>
+                            <h2> Users : </h2>
+                        <?php
+                        while ($row = mysqli_fetch_assoc($resultTabUsers))
+                        { 
+                        ?>
+                        <article>
+                            <h3><?= $row['alias'] ?></h3>                    
+                        </article>
+                        <?php
+                        }}
+                    if ($queryResultBoardgames > 0){
+                        ?>
+                        <h2> Jeux : </h2>
+                        <?php
+                        while ($row = mysqli_fetch_assoc($resultTabBoardgames))
+                        { 
+                            ?>
+                                <article>
+                                <section id='articleJeux'>
+                                    <div>
+                                        <h1><?=$row['name']?></h1>
+                                        <small>Créateur•ice : <?=$row['creator']?></small>
+                                        <p><strong>Age minimum : <?=$row['min_age']?>ans • Nombre de joueur•euse•s : <?=$row['min_players']?> - <?=$row['max_players']?> • Durée d'une partie : <?=$row['duration']?>minutes </strong></p> 
+                                        <br>
+                                        <p><?=$row['description']?></p>
+                                    </div>
+                                    <br>
+                                    <div>
+                                        <img alt='Image du jeu<?=$row['name']?>' height='300' src = <?=$row['images']?>></img>
+                                    </div>
+                                </section>
+                                <footer>
+                                    <form action="addToLudotheque.php" method="POST">
+                                        <button type="submit" name="submit-like" value=<?=$row['id']?> >♥</button>
+                                    </form>
+                                    <p>Type: <i><?=$row['type']?></i></p>  
+                                </footer>
+                            </article>
+                            <?php
+                        }}
+                    if ($queryResultPosts > 0){
+                        ?>
+                        <h2> Articles : </h2>
+                        <?php
+                        while ($row = mysqli_fetch_assoc($resultTabPosts))
+                        { 
+                            ?> 
+                                <article>
+                                    <h3>
+                                        <time><i><?= $row['created'] ?></i></time>
+                                    </h3>
+                                    <br>
+                                    <div>
+                                        <p><?= $row['content'] ?></p>
+                                    </div>
+                                    <div>
+                                        <p> <i>De : <?= $row['author_name'] ?></i></p><!--  Faut aller chercher dans une autre table  -->
+                                    </div>
+                                    <footer>
+                                    <small>♥</small>
+                                    </footer>
+                                </article>
+                            <?php
+                        }}
                 }else {
                     echo "Pas de résultat pour votre recherche !";
                 }        
