ba3d91f3e083c84e8f4c23f38700405b0639bb5e
abj95630 <abah20000@gmail.com>
2023-02-23 12:42:58 +0100

    Restriction pour la page admin réservé à l'administrateur, correction pour la connexion et modification header

--- projet/admin.php
+++ projet/admin.php
@@ -1,6 +1,9 @@
 <?php
-            include 'header.php';
-        ?>
+        include 'header.php';
+        if(!isset($_SESSION['admin'])) {
+            header("Location:news.php");
+        }
+?>
     </head>
     <body>
         <?php
--- projet/connexion.php
+++ projet/connexion.php
@@ -1,42 +1,3 @@
-<?php 
-    session_start();
-    if(isset($_SESSION['connected_id'])) {
-        header("Location: news.php");
-    }
-    
-    include 'db.php';
-    $enCoursDeTraitement = isset($_POST['email']);
-    if ($enCoursDeTraitement)
-    {
-        $emailAVerifier = $_POST['email'];
-        $passwdAVerifier = $_POST['motpasse'];
-
-        //Etape 4 : Petite sécurité
-        // pour éviter les injection sql : https://www.w3schools.com/sql/sql_injection.asp
-        $emailAVerifier = $mysqli->real_escape_string($emailAVerifier);
-        $passwdAVerifier = $mysqli->real_escape_string($passwdAVerifier);
-        // on crypte le mot de passe pour éviter d'exposer notre utilisatrice en cas d'intrusion dans nos systèmes
-        $passwdAVerifier = md5($passwdAVerifier);
-        // NB: md5 est pédagogique mais n'est pas recommandée pour une vraies sécurité
-        //Etape 5 : construction de la requete
-        $lInstructionSql = "SELECT * "
-                . "FROM users "
-                . "WHERE "
-                . "email LIKE '" . $emailAVerifier . "'";
-        // Etape 6: Vérification de l'utilisateur
-        $res = $mysqli->query($lInstructionSql);
-        $user = $res->fetch_assoc();
-        if (! $user OR $user["password"] != $passwdAVerifier)
-        {
-            echo "La connexion a échouée. ";
-            
-        } else
-        {
-            $_SESSION['connected_id'] = $user['id'];
-            header("Location:discover.php");
-        }
-    }
-?> 
 <!doctype html>
 <html lang="fr">
     <head>
@@ -52,7 +13,49 @@
             </aside>
             <main>
                 <article>
-                    <h2>Connexion</h2>                    
+                    <h2>Connexion</h2>  
+                        <?php 
+                        session_start();
+                        if(isset($_SESSION['connected_id'])) {
+                            header("Location: news.php");
+                        }
+                        
+                        include 'db.php';
+                        $enCoursDeTraitement = isset($_POST['email']);
+                        if ($enCoursDeTraitement)
+                        {
+                            $emailAVerifier = $_POST['email'];
+                            $passwdAVerifier = $_POST['motpasse'];
+
+                            //Etape 4 : Petite sécurité
+                            // pour éviter les injection sql : https://www.w3schools.com/sql/sql_injection.asp
+                            $emailAVerifier = $mysqli->real_escape_string($emailAVerifier);
+                            $passwdAVerifier = $mysqli->real_escape_string($passwdAVerifier);
+                            // on crypte le mot de passe pour éviter d'exposer notre utilisatrice en cas d'intrusion dans nos systèmes
+                            $passwdAVerifier = md5($passwdAVerifier);
+                            // NB: md5 est pédagogique mais n'est pas recommandée pour une vraies sécurité
+                            //Etape 5 : construction de la requete
+                            $lInstructionSql = "SELECT * "
+                                    . "FROM users "
+                                    . "WHERE "
+                                    . "email LIKE '" . $emailAVerifier . "'";
+                            // Etape 6: Vérification de l'utilisateur
+                            $res = $mysqli->query($lInstructionSql);
+                            $user = $res->fetch_assoc();
+                            if (! $user OR $user["password"] != $passwdAVerifier)
+                            {
+                                echo "La connexion a échouée. ";
+                                
+                            } else
+                            {
+                                if($emailAVerifier == "admin@admin.fr") {
+                                    $_SESSION['admin'] = "admin";
+                                }
+                                $_SESSION['connected_id'] = $user['id'];
+                                header("Location:discover.php");
+                            }
+                        }
+                    ?>                   
                     <form action="connexion.php" method="post">
                         <dl>
                             <dt><label for='email'>E-Mail</label></dt>
